b51db6fc493a172005a5c7408935ebf68873609e
mironal <miro.gunp@gmail.com>
2013-04-16 23:35:37 +0900

    タイムテーブルをｱﾚするｱﾚを書いた.

---
+++ hyper-miuchi/2013-04-17/timetable/Timetable.scala
@@ -0,0 +1,15 @@
+case class Talk(name: String, title: String, timeMin: Int) {
+    override def toString = {
+        s"$name : $title : $timeMin 分"
+    }
+}
+
+scala.util.Random.shuffle(
+    Talk("ｷｬﾒﾁｬﾝ", "画像フィルタ入門", 40) ::
+    Talk("mtmta", "iPhone アプリのライブコーディング", 40) ::
+    Talk("ｸｿｼｰ", "", 40) ::
+    Talk("mironal", "Scala入門 クラス編", 40) ::
+    Talk("mironal", "CUIなｱﾚ", 15) ::
+    Talk("mironal", "arduinodeの紹介", 10) ::
+    Nil
+).foreach(println)
