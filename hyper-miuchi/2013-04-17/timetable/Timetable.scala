9811fc2a0e1b42d70a18d16785b517a4cf8a6384
mironal <miro.gunp@gmail.com>
2013-04-17 06:50:17 +0900

    ｸｿｼｰ追加

--- hyper-miuchi/2013-04-17/timetable/Timetable.scala
+++ hyper-miuchi/2013-04-17/timetable/Timetable.scala
@@ -7,7 +7,7 @@ case class Talk(name: String, title: String, timeMin: Int) {
 scala.util.Random.shuffle(
     Talk("ｷｬﾒﾁｬﾝ", "画像フィルタ入門", 40) ::
     Talk("mtmta", "iPhone アプリのライブコーディング", 40) ::
-    Talk("ｸｿｼｰ", "", 40) ::
+    Talk("ｸｿｼｰ", "ゲーム機のユーザーインターフェースの歴史について", 15) ::
     Talk("mironal", "Scala入門 クラス編", 40) ::
     Talk("mironal", "CUIなｱﾚ", 15) ::
     Talk("mironal", "arduinodeの紹介", 10) ::
